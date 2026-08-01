230df0770f7a862d3e414ece5ed1cea894233545
noah057757 <noazeiraa@gmail.com>
2026-08-01 20:14:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
-<script src="script.js"></script>
+const carrinho = [];
+const areaCarrinho = document.getElementById("carrinho");
+
+function atualizarCarrinho() {
+
+    let html = "<h2>🛒 Carrinho</h2>";
+
+    if (carrinho.length === 0) {
+        html += "<p>Seu carrinho está vazio.</p>";
+        areaCarrinho.innerHTML = html;
+        return;
+    }
+
+    let total = 0;
+
+    carrinho.forEach((item, indice) => {
+
+        total += item.preco;
+
+        html += `
+        <p>
+        ${item.nome} - R$ ${item.preco.toFixed(2)}
+        <button onclick="removerItem(${indice})">❌</button>
+        </p>
+        `;
+
+    });
+
+    html += `<h3>Total: R$ ${total.toFixed(2)}</h3>`;
+
+    areaCarrinho.innerHTML = html;
+
+}
+
+function removerItem(indice){
+
+    carrinho.splice(indice,1);
+
+    atualizarCarrinho();
+
+}
+
+document.querySelectorAll(".prato").forEach(prato=>{
+
+    const botao = prato.querySelector("button");
+
+    botao.addEventListener("click",()=>{
+
+        const nome = prato.querySelector("h3").innerText;
+
+        const precoTexto = prato.querySelector("strong").innerText;
+
+        const preco = Number(
+            precoTexto
+            .replace("R$","")
+            .replace(",",".")
+            .trim()
+        );
+
+        carrinho.push({
+            nome,
+            preco
+        });
+
+        atualizarCarrinho();
+
+    });
+
+});
+
+atualizarCarrinho();
